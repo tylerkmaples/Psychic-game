318cdaa91ce189a8589b67c4b223030743d9620a
TKMCode <tyler.code.maples@gmail.com>
2018-11-07 20:51:07 -0800

    not many changes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,7 +7,6 @@ var wrongGuessElem = document.getElementById("wrongguesses");
 var wins = 0;
 var losses = 0;
 var guesses = 9;
-var wrong = "";
 var cpuChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 // Get computer's randomly generated letter: (should this be a function which i then invoke when win/loss happens?)
@@ -28,11 +27,10 @@ document.onkeyup = function (e) {
         alert(userInput   +  " is correct. You win!");
         wins++;
     }
-    // lose game 
+    // guess incorrectly
     else if (aiGuess !== userInput) {
         alert(userInput + " is incorrect. Please try again.");
         guesses--;
-
     }
 
     winsElem.textContent = wins;

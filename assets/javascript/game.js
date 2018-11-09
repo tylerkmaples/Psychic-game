6c8f0249fab669ec8cf7294081d0d7c7bc06b806
TKMCode <tyler.code.maples@gmail.com>
2018-11-08 18:38:54 -0800

    added functions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,6 +7,7 @@ var wrongGuessElem = document.getElementById("wrongguesses");
 var wins = 0;
 var losses = 0;
 var guesses = 9;
+var wrongGuess = [];
 var cpuChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 // Get computer's randomly generated letter: (should this be a function which i then invoke when win/loss happens?)
@@ -14,6 +15,27 @@ var cpuChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m
 var randomChar = Math.floor(Math.random() * cpuChoices.length);
 var aiGuess = cpuChoices[randomChar];
 console.log(aiGuess);
+
+// function to check the guesses
+
+
+var check = function () {
+    if (wrongGuess.length === 9 ){
+        alert("You are out of lives. Game Over!");
+        reset();
+    }
+    else if (wrongGuess.length !==9) {
+        wrongGuess.push(userInput);
+        wrongGuessesElem.textContent = wrongGuess; 
+    }
+}
+
+// reset the game when win/lose
+
+var reset = function() {
+
+}
+
 
 
 // get user guess
@@ -25,16 +47,20 @@ document.onkeyup = function (e) {
     // win game
     if (aiGuess === userInput){
         alert(userInput   +  " is correct. You win!");
+        reset();
         wins++;
+        
     }
     // guess incorrectly
     else if (aiGuess !== userInput) {
         alert(userInput + " is incorrect. Please try again.");
         guesses--;
+        check();
+        
     }
 
     winsElem.textContent = wins;
     lossesElem.textContent = losses;
     guessesElem.textContent = guesses;
-    wrongGuessElem.textcontent = wrong;
+    // wrongGuessElem.textcontent = wrong;
 }

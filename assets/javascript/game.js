579db1e638e9841a5205517ba5c15273b7a1e50d
TKMCode <tyler.code.maples@gmail.com>
2018-11-09 22:13:36 -0800

    just need to add capability for if else to read new letters

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -11,32 +11,35 @@ var wrongGuess = [];
 var cpuChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 // Get computer's randomly generated letter: (should this be a function which i then invoke when win/loss happens?)
-
-var randomChar = Math.floor(Math.random() * cpuChoices.length);
-var aiGuess = cpuChoices[randomChar];
-console.log(aiGuess);
-
-// function to check the guesses
-
-
-var check = function () {
-    if (wrongGuess.length === 9 ){
-        alert("You are out of lives. Game Over!");
-        reset();
-    }
-    else if (wrongGuess.length !==9) {
-        wrongGuess.push(userInput);
-        wrongGuessesElem.textContent = wrongGuess; 
-    }
-}
+// var aiGuess = function () {
+//     var randomGuess = Math.floor(Math.random() * cpuChoices.length);
+//     cpuChoices[randomGuess];
+    
+// }
+    // var randomChar = Math.floor(Math.random() * cpuChoices.length);
+    // var aiGuess = cpuChoices[randomChar];
+    // console.log(aiGuess);
 
 // reset the game when win/lose
 
-var reset = function() {
-
+var startGuess = function() {
+    wrongGuess = [];
+    guesses = 9;
+    var randomChar = Math.floor(Math.random() * cpuChoices.length);
+    var aiGuess = cpuChoices[randomChar];
+    console.log(aiGuess);
+    return(aiGuess);
 }
 
+startGuess();
 
+function getVariable(){
+    return(startGuess);
+}
+
+var aiGuess = getVariable();
+
+// startGuess();
 
 // get user guess
 
@@ -47,20 +50,33 @@ document.onkeyup = function (e) {
     // win game
     if (aiGuess === userInput){
         alert(userInput   +  " is correct. You win!");
-        reset();
         wins++;
+        startGuess();
+        
         
     }
     // guess incorrectly
-    else if (aiGuess !== userInput) {
+    else if (aiGuess !== userInput && wrongGuess.indexOf(userInput) > -1){
+        alert("You already guessed that! Please try another letter.");
+
+    }
+
+    else if (aiGuess !== userInput && wrongGuess.indexOf(userInput) < 0 && guesses > 1){                         
         alert(userInput + " is incorrect. Please try again.");
         guesses--;
-        check();
+        wrongGuess.push(userInput);
+        console.log(wrongGuess);
         
+    }
+
+    else {
+        alert(userInput + " is incorrect. Game Over!");
+        losses++;
+        startGuess();
     }
 
     winsElem.textContent = wins;
     lossesElem.textContent = losses;
     guessesElem.textContent = guesses;
-    // wrongGuessElem.textcontent = wrong;
+    wrongGuessElem.textContent = wrongGuess;
 }
